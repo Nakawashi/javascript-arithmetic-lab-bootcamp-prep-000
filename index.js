97a65bf1fa3f82921a0f03f5aa839f86b2dc5e84
Lucie Genevey <l.genevey@gmail.com>
2017-09-26 19:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-function add(a, b) {
-  return a += b
+
+function add(a,b) {
+  return a+=b
 }
 
-function substract(a, b) {
-  return a -= b
+function subtract(a,b) {
+  return a-=b
 }
 
-function multiply(a, b) {
-  return a *= b
+function multiply(a,b) {
+  return a*=b
 }
 
-function divide(a, b) {
-  return a /= b
+function divide(a,b) {
+  return a/=b
 }
 
-function inc(a) {
-  return a ++
+function inc(n) {
+  return n+=1
 }
 
-function dec(a) {
-  return a --
+function dec(n) {
+  return n-=1
 }
 
 function makeInt(n) {
-  if (n === parseInt(n)) {
-    return n
-  }
-  else if (n === parseInt(n, 10)) {
-    return n
-  }
-  else if (isNaN(n)) {
-    return true
-  }
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
